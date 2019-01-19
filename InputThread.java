352f511ab61d61623018dc79258e4a62f549fa08
mastlouis <mastlouis@wpi.edu>
2019-01-19 15:39:05 -0500

    Update InputThread.java

--- InputThread.java
+++ InputThread.java
@@ -1,21 +1,27 @@
 //package libs;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
 import com.jsyn.unitgen.LineOut;
 
 public class InputThread extends Thread {
-	Scanner keyboard;
-	boolean quit;
-	String nextLine;
-	char nextCommand;
-	LineOut lineOut;
+	private Scanner keyboard;
+	private boolean quit;
+	private String nextLine;
+	private char nextCommand;
+	private LineOut lineOut;
+	
+	private double pitchOffset;
+	private double volumeOffset;
 	
 	public InputThread(LineOut lineOut) {
 		this.keyboard = new Scanner(System.in);
 		this.quit = false;
 		this.lineOut = lineOut;
+		pitchOffset = 200.0;
+		volumeOffset = 0.05;
 	}
 	
 	public void run() {
@@ -32,7 +38,15 @@ public class InputThread extends Thread {
 				break;
 			case('q'):
 				lineOut.stop();
-				lineOut.start(3.0);
+				System.out.println("Quitting");
+				try{
+					TimeUnit.SECONDS.sleep(5);
+				}
+				catch(InterruptedException e){
+					System.out.println("Whoop, there it is.");
+				}
+				System.out.println("There is no escape.");
+				lineOut.start();
 				break;
 			default:
 				System.out.println("Invalid command.");
@@ -44,8 +58,22 @@ public class InputThread extends Thread {
 		
 	}
 	
+	private void raisePitch() {
+		if(this.pitchOffset < 400) {
+			pitchOffset += 50;
+		}
+	}
+	
 	public boolean isRunning() {
 		return !this.quit;
+	}
+	
+	public double getVolumeOffset() {
+		return this.volumeOffset;
+	}
+	
+	public double getPitchOffset() {
+		return this.pitchOffset;
 	}
 
 }
