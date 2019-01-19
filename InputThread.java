d4af76038f45e6e8b806453a0dc7e556088625f9
mastlouis <mastlouis@wpi.edu>
2019-01-19 15:09:06 -0500

    Update InputThread.java

--- InputThread.java
+++ InputThread.java
@@ -1,34 +1,51 @@
-package libs;
+//package libs;
 import java.util.Scanner;
 
-public class InputThread implements Runnable {
+import com.jsyn.JSyn;
+import com.jsyn.Synthesizer;
+import com.jsyn.unitgen.LineOut;
+
+public class InputThread extends Thread {
 	Scanner keyboard;
 	boolean quit;
 	String nextLine;
 	char nextCommand;
+	LineOut lineOut;
 	
-	public InputThread() {
-		keyboard = new Scanner(System.in);
-		quit = false;
+	public InputThread(LineOut lineOut) {
+		this.keyboard = new Scanner(System.in);
+		this.quit = false;
+		this.lineOut = lineOut;
 	}
 	
 	public void run() {
 		while(!quit) {
-			nextLine = keyboard.nextLine();
-			nextCommand = nextLine.charAt(0);
+			this.nextLine = keyboard.nextLine();
+			this.nextCommand = nextLine.charAt(0);
 			
-			switch(nextCommand) {
-			case('q'):
-				quit = true;
+			switch(this.nextCommand) {
+			case('s'):
+				this.quit = true;
 				break;
 			case('n'):
 				System.out.println("Never gonna give you up.");
+				break;
+			case('q'):
+				lineOut.stop();
+				lineOut.start(3.0);
 				break;
 			default:
 				System.out.println("Invalid command.");
 			}
 		}
+		System.out.println("Closing thread.");
+		return;
+		//System.out.println("You forgot to kill your children.");
 		
+	}
+	
+	public boolean isRunning() {
+		return !this.quit;
 	}
 
 }
