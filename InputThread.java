98e82ef1d5f778c614f97f137d91a279b2c96f58
mastlouis <mastlouis@wpi.edu>
2019-01-19 17:26:24 -0500

    Update InputThread.java

--- InputThread.java
+++ InputThread.java
@@ -27,41 +27,44 @@ public class InputThread extends Thread {
 	public void run() {
 		while(!quit) {
 			this.nextLine = keyboard.nextLine();
-			this.nextCommand = nextLine.charAt(0);
-			
-			switch(this.nextCommand) {
-			case('s'):
-				this.quit = true;
-			break;
-			case('n'):
-				System.out.println("Never gonna give you up.");
-			break;
-			case('p'):
-				this.raisePitch();
-			break;
-			case('P'):
-				this.lowerPitch();
-			break;
-			case('v'):
-				this.raiseVolume();
-			break;
-			case('V'):
-				this.lowerVolume();
-			break;
-			case('q'):
-				lineOut.stop();
-				System.out.println("Quitting");
-				try{
-					TimeUnit.SECONDS.sleep(5);
+			while(nextLine.length() > 0) {
+				this.nextCommand = nextLine.charAt(0);
+				this.nextLine = this.nextLine.substring(1);
+				
+				switch(this.nextCommand) {
+				case('s'):
+					this.quit = true;
+				break;
+				case('n'):
+					System.out.println("Never gonna give you up.");
+				break;
+				case('p'):
+					this.raisePitch();
+				break;
+				case('P'):
+					this.lowerPitch();
+				break;
+				case('v'):
+					this.raiseVolume();
+				break;
+				case('V'):
+					this.lowerVolume();
+				break;
+				case('q'):
+					lineOut.stop();
+					System.out.println("Quitting");
+					try{
+						TimeUnit.SECONDS.sleep(5);
+					}
+					catch(InterruptedException e){
+						System.out.println("Whoop, there it is.");
+					}
+					System.out.println("There is no escape.");
+					lineOut.start();
+				break;
+				default:
+					System.out.println("Invalid command.");
 				}
-				catch(InterruptedException e){
-					System.out.println("Whoop, there it is.");
-				}
-				System.out.println("There is no escape.");
-				lineOut.start();
-			break;
-			default:
-				System.out.println("Invalid command.");
 			}
 		}
 		System.out.println("Closing thread.");
@@ -71,26 +74,26 @@ public class InputThread extends Thread {
 	}
 	
 	private void raisePitch() {
-		if(this.pitchOffset < 600) {
-			pitchOffset += 1000;
+		if(this.pitchOffset < 2000) {
+			pitchOffset *= 2;
 		}
 	}
 	
 	private void lowerPitch() {
 		if(this.pitchOffset > 100) {
-			pitchOffset -= 100;
+			pitchOffset /= 2;
 		}
 	}
 	
 	private void raiseVolume() {
-		if(this.pitchOffset < 0.15) {
-			pitchOffset += 0.02;
+		if(this.volumeOffset < 0.15) {
+			volumeOffset += 0.05;
 		}
 	}
 	
 	private void lowerVolume() {
-		if(this.pitchOffset > 0.05) {
-			pitchOffset -= 0.02;
+		if(this.volumeOffset > 0.05) {
+			volumeOffset -= 0.05;
 		}
 	}
 	
