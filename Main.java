08b9ccc08e7bed2f014295f4d2581425b6bc39fe
henryposkanzer <hposkanzer@gmail.com>
2019-01-19 11:01:22 -0500

    nothing

--- Main.java
+++ Main.java
@@ -2,9 +2,16 @@ import java.awt.MouseInfo;
 import java.awt.PointerInfo;
 import java.awt.Point;
 import java.util.concurrent.TimeUnit;
+import javax.sound.sampled.*;
 
 class Main{
+	public static final int SAMPLE_RATE = 10;
+	public static final int BYTES_PER_SAMPLE = 1;
+	public static final int BITS_PER_SAMPLE = BYTES_PER_SAMPLE * 8;
+	public static final int SAMPLE_BUFFER_SIZE = 1000;
+
 	public static void main(String args[]){
+		
 		System.out.println("Press q to quit.");
 		double newX, newY, oldX, oldY, speed;
 		oldX = oldY = 0;
